fa1a4b7bf9ec38cd29b389ba523aeb012baecb8e
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-28 00:53:20 +0530

    ✅ Add TUF — Your Entire Interview PrepJourney Simplified in ONE STOP

---
+++ TUF/your-entire-interview-prepjourney-simplified-in-one-stop/solution.cpp
@@ -0,0 +1,10 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : TUF
+// Problem   : Your Entire Interview PrepJourney Simplified in ONE STOP
+// Difficulty: Unknown
+// Language  : cpp
+// URL       : https://takeuforward.org/plus
+// Solved    : 2026-05-27T19:23:19.551Z
+// ────────────────────────────────────────────────────────────
+
+// Paste your solution here.
