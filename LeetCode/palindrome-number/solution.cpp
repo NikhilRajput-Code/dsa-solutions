bb28257f80d67aa797bdd68105d21e7c73eed58e
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-28 00:51:56 +0530

    ✅ Add LeetCode — Palindrome Number

---
+++ LeetCode/palindrome-number/solution.cpp
@@ -0,0 +1,10 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : LeetCode
+// Problem   : Palindrome Number
+// Difficulty: Easy
+// Language  : cpp
+// URL       : https://leetcode.com/problems/palindrome-number/description/
+// Solved    : 2026-05-27T19:21:55.131Z
+// ────────────────────────────────────────────────────────────
+
+// Paste solution here.
