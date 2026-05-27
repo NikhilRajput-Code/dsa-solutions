c81c4a29d1b9963e1001f80da7b1721b418c64b7
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-28 00:53:37 +0530

    ✅ Add TUF — Command Palette

---
+++ TUF/command-palette/solution.java
@@ -0,0 +1,24 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : TUF
+// Problem   : Command Palette
+// Difficulty: Unknown
+// Language  : java
+// URL       : https://takeuforward.org/plus/dsa/problems/second-largest-element?subject=dsa&approach=optimal&tab=submissions
+// Solved    : 2026-05-27T19:23:36.521Z
+// ────────────────────────────────────────────────────────────
+
+class Solution {
+    public int secondLargestElement(int[] nums) {
+    int n = nums.length;
+    if(n < 2) {
+      return -1;
+    }
+      Arrays.sort(nums);
+      int largest = nums[n-1];
+      int secondLargest = -1;
+      for(int i = n-1;i>=0;i--) {
+        if(nums[i] != largest) {
+          secondLargest = nums[i];
+          break;
+        }
+      
