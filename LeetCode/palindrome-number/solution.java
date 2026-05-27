f7e2741641fe80b83372c404cc8507c13faed7d9
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-28 00:52:01 +0530

    ✅ Add LeetCode — Palindrome Number

---
+++ LeetCode/palindrome-number/solution.java
@@ -0,0 +1,23 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : LeetCode
+// Problem   : Palindrome Number
+// Difficulty: Easy
+// Language  : java
+// URL       : https://leetcode.com/problems/palindrome-number/description/
+// Solved    : 2026-05-27T19:22:00.050Z
+// ────────────────────────────────────────────────────────────
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        return (x == reversed || x == reversed / 10);
+    }
+}
