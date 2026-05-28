5667680a25968e958d34bbd5b9d31eb8568cdd26
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-28 22:26:41 +0530

    ✅ Add LeetCode — Valid Parentheses

--- LeetCode/valid-parentheses/solution.java
+++ LeetCode/valid-parentheses/solution.java
@@ -4,7 +4,7 @@
 // Difficulty: Easy
 // Language  : java
 // URL       : https://leetcode.com/problems/valid-parentheses/
-// Solved    : 2026-05-28T16:56:31.301Z
+// Solved    : 2026-05-28T16:56:41.263Z
 // ────────────────────────────────────────────────────────────
 
                 char top = stack.pop();
