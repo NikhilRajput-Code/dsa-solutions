50903ac0025e9ee0750355bbdfec5c7bb96ca416
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-28 23:48:04 +0530

    ✅ Add TUF — Command Palette

--- TUF/command-palette/solution.java
+++ TUF/command-palette/solution.java
@@ -3,22 +3,21 @@
 // Problem   : Command Palette
 // Difficulty: Unknown
 // Language  : java
-// URL       : https://takeuforward.org/plus/dsa/problems/second-largest-element?subject=dsa&approach=optimal&tab=submissions
-// Solved    : 2026-05-27T19:24:11.630Z
+// URL       : https://takeuforward.org/plus/dsa/problems/traversal-in-linked-list?subject=dsa&tab=editorial
+// Solved    : 2026-05-28T18:18:03.055Z
 // ────────────────────────────────────────────────────────────
 
+*/
+
 class Solution {
-    public int secondLargestElement(int[] nums) {
-    int n = nums.length;
-    if(n < 2) {
-      return -1;
-    }
-      Arrays.sort(nums);
-      int largest = nums[n-1];
-      int secondLargest = -1;
-      for(int i = n-1;i>=0;i--) {
-        if(nums[i] != largest) {
-          secondLargest = nums[i];
-          break;
+    public List<Integer> LLTraversal(ListNode head) {
+        //your code goes here
+        List<Integer> ans = new ArrayList<>();
+        while(temp != null) {
+            ans.add(temp.data);
+            temp = temp.next;
         }
-      
+        return ans;
+    }
+        ListNode temp = head;
+}
