5ff396947409fbba06e65831369d190437471fda
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-28 23:54:04 +0530

    ✅ Add TUF — Command Palette

--- TUF/command-palette/solution.java
+++ TUF/command-palette/solution.java
@@ -3,21 +3,22 @@
 // Problem   : Command Palette
 // Difficulty: Unknown
 // Language  : java
-// URL       : https://takeuforward.org/plus/dsa/problems/traversal-in-linked-list?subject=dsa&tab=submissions
-// Solved    : 2026-05-28T18:18:30.089Z
+// URL       : https://takeuforward.org/plus/dsa/problems/deletion-of-the-head-of-ll?category=linked-list&subcategory=fundamentals-single-ll&tab=editorial
+// Solved    : 2026-05-28T18:24:02.882Z
 // ────────────────────────────────────────────────────────────
 
-*/
-
-class Solution {
-    public List<Integer> LLTraversal(ListNode head) {
-        //your code goes here
-        List<Integer> ans = new ArrayList<>();
-        while(temp != null) {
-            ans.add(temp.data);
-            temp = temp.next;
-        }
-        return ans;
     }
-        ListNode temp = head;
+
+    ListNode(int x, ListNode next) {
+        this.data = x;
+        this.next = next;
+    }
 }
+*/
+class Solution {
+    public ListNode deleteHead(ListNode head) {
+        ListNode temp = head;
+        head = head.next;
+        temp = null;
+        if(head == null) 
+        return null;
