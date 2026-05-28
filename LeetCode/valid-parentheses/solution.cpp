40edeb9e271cb455931fe4fed0d21a14e6a3e342
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-28 22:25:57 +0530

    ✅ Add LeetCode — Valid Parentheses

---
+++ LeetCode/valid-parentheses/solution.cpp
@@ -0,0 +1,10 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : LeetCode
+// Problem   : Valid Parentheses
+// Difficulty: Easy
+// Language  : cpp
+// URL       : https://leetcode.com/problems/valid-parentheses/description/
+// Solved    : 2026-05-28T16:55:55.390Z
+// ────────────────────────────────────────────────────────────
+
+// Paste solution here.
