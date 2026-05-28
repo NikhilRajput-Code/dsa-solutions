4ba92e461c35ae2eed2a8786bf27056c7c64afe5
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-28 23:56:22 +0530

    ✅ Add TUF — Command Palette

--- TUF/command-palette/solution.java
+++ TUF/command-palette/solution.java
@@ -4,7 +4,7 @@
 // Difficulty: Unknown
 // Language  : java
 // URL       : https://takeuforward.org/plus/dsa/problems/deletion-of-the-head-of-ll?category=linked-list&subcategory=fundamentals-single-ll&tab=submissions
-// Solved    : 2026-05-28T18:26:11.416Z
+// Solved    : 2026-05-28T18:26:21.416Z
 // ────────────────────────────────────────────────────────────
 
         this.next = next;
