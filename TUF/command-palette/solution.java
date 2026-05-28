e8a30e7e239e4c63252235047626ed78463de70a
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-29 00:03:11 +0530

    ✅ Add TUF — Command Palette

--- TUF/command-palette/solution.java
+++ TUF/command-palette/solution.java
@@ -4,7 +4,7 @@
 // Difficulty: Unknown
 // Language  : java
 // URL       : https://takeuforward.org/plus/dsa/problems/deletion-of-the-tail-of-ll?category=linked-list&subcategory=fundamentals-single-ll
-// Solved    : 2026-05-28T18:33:01.714Z
+// Solved    : 2026-05-28T18:33:10.118Z
 // ────────────────────────────────────────────────────────────
 
 class Solution {
