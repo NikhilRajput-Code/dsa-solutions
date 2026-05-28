2c76e26acefe1f13acd9356e26cec33e966e69cf
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-28 22:27:41 +0530

    ✅ Add LeetCode — Valid Parentheses

--- LeetCode/valid-parentheses/solution.java
+++ LeetCode/valid-parentheses/solution.java
@@ -3,23 +3,8 @@
 // Problem   : Valid Parentheses
 // Difficulty: Easy
 // Language  : java
-// URL       : https://leetcode.com/problems/valid-parentheses/editorial/
-// Solved    : 2026-05-28T16:57:30.807Z
+// URL       : https://leetcode.com/problems/valid-parentheses/description/
+// Solved    : 2026-05-28T16:57:41.303Z
 // ────────────────────────────────────────────────────────────
 
-                char top = stack.pop();
-                if (ch == ')' && top != '(') {
-                    return false;
-                }
-                if (ch == ']' && top != '[') {
-                    return false;
-                }
-                    return false;
-                if (stack.isEmpty()) {
-            } else {
-                stack.push(ch);
-            if (ch == '(' || ch == '[' || ch == '{') {
-        for (char ch : s.toCharArray()) {
-        Stack<Character> stack = new Stack<>();
-class Solution {
-    public boolean isValid(String s) {
+c
