412358ad7a25e0015f284e48573a8ae1222ec0eb
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-28 23:27:36 +0530

    ✅ Add TUF — Everything You  Need To Crack Tech Interviews

---
+++ TUF/everything-you-need-to-crack-tech-interviews/solution.cpp
@@ -0,0 +1,12 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : TUF
+// Problem   : Everything You 
+ Need To Crack
+Tech Interviews
+// Difficulty: Unknown
+// Language  : cpp
+// URL       : https://takeuforward.org/
+// Solved    : 2026-05-28T17:57:17.085Z
+// ────────────────────────────────────────────────────────────
+
+// Paste your solution here.
