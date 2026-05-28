92b7eb8e25f90d75484e06bb29810370d626ebe5
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-29 00:04:52 +0530

    ✅ Add TUF — Command Palette

--- TUF/command-palette/solution.java
+++ TUF/command-palette/solution.java
@@ -3,21 +3,21 @@
 // Problem   : Command Palette
 // Difficulty: Unknown
 // Language  : java
-// URL       : https://takeuforward.org/plus/dsa/problems/deletion-of-the-tail-of-ll?category=linked-list&subcategory=fundamentals-single-ll&tab=submissions
-// Solved    : 2026-05-28T18:33:30.008Z
+// URL       : https://takeuforward.org/plus/dsa/problems/deletion-of-the-head-of-ll?subject=dsa&tab=submissions
+// Solved    : 2026-05-28T18:34:50.307Z
 // ────────────────────────────────────────────────────────────
 
+        this.next = next;
+    }
+}
+*/
 class Solution {
-    public ListNode deleteTail(ListNode head) {
-        //your code goes here
-        if(head == null || head.next == null) {
-            return null;
-        }
+    public ListNode deleteHead(ListNode head) {
+        if(head == null) 
+        return null;
         ListNode temp = head;
-        while(temp.next.next != null) {
-            temp = temp.next;
-        }
-        temp.next = null;
+        head = head.next;
+        temp = null;
         return head;
     }
 }
