f05c28337faabb036e9c0dd14de25933bad302cd
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-28 22:26:11 +0530

    ✅ Add LeetCode — Valid Parentheses

---
+++ LeetCode/valid-parentheses/solution.java
@@ -0,0 +1,14 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : LeetCode
+// Problem   : Valid Parentheses
+// Difficulty: Easy
+// Language  : java
+// URL       : https://leetcode.com/problems/valid-parentheses/solutions/7108838/100-beats-0ms-stack-beginner-friendly-py-y4xp/
+// Solved    : 2026-05-28T16:56:10.320Z
+// ────────────────────────────────────────────────────────────
+
+class Solution {
+    public boolean isValid(String s) {
+        
+    }
+}
