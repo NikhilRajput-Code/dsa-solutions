5c0bf21fb165884aba7abdf12edd6584d9cbfd1b
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-29 10:23:21 +0530

    ✅ Add GFG — Deletion at the end of a Linked List | Practice | GeeksforGeeks

--- GFG/deletion-at-the-end-of-a-linked-list-practice-geeksforgeeks/solution.cpp
+++ GFG/deletion-at-the-end-of-a-linked-list-practice-geeksforgeeks/solution.cpp
@@ -4,7 +4,7 @@
 // Difficulty: Unknown
 // Language  : cpp
 // URL       : https://www.geeksforgeeks.org/problems/deletion-at-the-end-of-a-linked-list/1
-// Solved    : 2026-05-29T04:53:15.096Z
+// Solved    : 2026-05-29T04:53:20.734Z
 // ────────────────────────────────────────────────────────────
 
 /*class Node{    int data;    Node next;    Node(int data)    {        this.data = data;        this.next = next;    }}*/class Solution {    public Node removeLastNode(Node head) {      if(head==null || head.next == null) {        return null;      }        Node temp = head;        while(temp.next.next != null) {            temp = temp.next;        }        temp.next = null;        return head;    }}
