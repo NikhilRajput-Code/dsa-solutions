fcb120f7431a8daafee3b66399258e31a26c0716
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-29 10:41:28 +0530

    ✅ Add LeetCode — Delete Node in a Linked List

--- LeetCode/delete-node-in-a-linked-list/solution.java
+++ LeetCode/delete-node-in-a-linked-list/solution.java
@@ -4,7 +4,7 @@
 // Difficulty: Medium
 // Language  : java
 // URL       : https://leetcode.com/problems/delete-node-in-a-linked-list/
-// Solved    : 2026-05-29T05:10:27.482Z
+// Solved    : 2026-05-29T05:11:27.473Z
 // ────────────────────────────────────────────────────────────
 
  *     int val;
