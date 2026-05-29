2a56a4f446566f1c18bf4a514c546e80532268e0
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-29 10:08:11 +0530

    ✅ Add GFG — LinkedList Traversal | Practice | GeeksforGeeks

--- GFG/linkedlist-traversal-practice-geeksforgeeks/solution.cpp
+++ GFG/linkedlist-traversal-practice-geeksforgeeks/solution.cpp
@@ -4,7 +4,7 @@
 // Difficulty: Unknown
 // Language  : cpp
 // URL       : https://www.geeksforgeeks.org/problems/linkedlist-traversal/1
-// Solved    : 2026-05-29T04:38:00.414Z
+// Solved    : 2026-05-29T04:38:10.416Z
 // ────────────────────────────────────────────────────────────
 
 #include <iostream>using namespace std;​// a linked list nodeclass Node {public:    int data;    Node* next;​    // constructor to initialize a new node with data    Node(int new_data) {        this->data = new_data;        this->next = nullptr;    }};​// function to traverse and print the singly linked listvoid traverseList(Node* head) {    while (head != nullptr) {        cout << head->data;        if (head->next != nullptr)              cout << " -> ";        head = head->next;    }    cout << endl;}​int main() {      // create a hard-coded linked list:    // 10 -> 20 -> 30 -> 40    Node* head = new Node(10);    head->next = new Node(20);    head->next->next = new Node(30);    head->next->next->next = new Node(40);​    traverseList(head);​    return 0;}
