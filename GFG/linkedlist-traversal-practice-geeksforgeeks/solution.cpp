4419cfbebafba4a2297632c4273ebb39b398817d
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-29 10:03:37 +0530

    ✅ Add GFG — LinkedList Traversal | Practice | GeeksforGeeks

---
+++ GFG/linkedlist-traversal-practice-geeksforgeeks/solution.cpp
@@ -0,0 +1,10 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : GFG
+// Problem   : LinkedList Traversal | Practice | GeeksforGeeks
+// Difficulty: Unknown
+// Language  : cpp
+// URL       : https://www.geeksforgeeks.org/problems/linkedlist-traversal/1
+// Solved    : 2026-05-29T04:33:35.666Z
+// ────────────────────────────────────────────────────────────
+
+/*class Node {    int data;    Node next;    Node(int val) {        data = val;        next = null;    }}*/class Solution {    void printList(Node head) {        // code here            }}
