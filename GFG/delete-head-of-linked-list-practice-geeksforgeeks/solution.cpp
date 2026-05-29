2611bd851eb6b0cb446be7d79e498fdffdde1354
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-29 10:17:59 +0530

    ✅ Add GFG — Delete Head of Linked List | Practice | GeeksforGeeks

---
+++ GFG/delete-head-of-linked-list-practice-geeksforgeeks/solution.cpp
@@ -0,0 +1,10 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : GFG
+// Problem   : Delete Head of Linked List | Practice | GeeksforGeeks
+// Difficulty: Unknown
+// Language  : cpp
+// URL       : https://www.geeksforgeeks.org/problems/delete-head-of-linked-list/1
+// Solved    : 2026-05-29T04:47:58.068Z
+// ────────────────────────────────────────────────────────────
+
+/*class Node{    int data;    Node next;    Node(int data)    {        this.data = data;        this.next = next;    }}*/class Solution {    public Node deleteHead(Node head) {        if(head == null)        return null;        Node temp = head;        head = head.next;        temp = null;        return head;    }}
