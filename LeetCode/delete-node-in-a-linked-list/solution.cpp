9e9de96735fbb5c5b75f044a783959875f1af4e9
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-29 10:29:43 +0530

    ✅ Add LeetCode — Delete Node in a Linked List

---
+++ LeetCode/delete-node-in-a-linked-list/solution.cpp
@@ -0,0 +1,10 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : LeetCode
+// Problem   : Delete Node in a Linked List
+// Difficulty: Medium
+// Language  : cpp
+// URL       : https://leetcode.com/problems/delete-node-in-a-linked-list/description/
+// Solved    : 2026-05-29T04:59:41.717Z
+// ────────────────────────────────────────────────────────────
+
+// Paste solution here.
