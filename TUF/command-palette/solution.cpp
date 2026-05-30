c25053bd8d7dced7f750a3f3fab415bf21d5515e
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-30 23:04:36 +0530

    ✅ Add TUF — Command Palette

---
+++ TUF/command-palette/solution.cpp
@@ -0,0 +1,10 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : TUF
+// Problem   : Command Palette
+// Difficulty: Unknown
+// Language  : cpp
+// URL       : https://takeuforward.org/plus/dsa/problems/merge-sorting?subject=dsa-concept-revision&sidebar=open
+// Solved    : 2026-05-30T17:34:33.470Z
+// ────────────────────────────────────────────────────────────
+
+// Paste your solution here.
