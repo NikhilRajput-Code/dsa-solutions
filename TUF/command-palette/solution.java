a6f5b3854bdb70cfd6e37152636928446217721f
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-30 23:37:06 +0530

    ✅ Add TUF — Command Palette

--- TUF/command-palette/solution.java
+++ TUF/command-palette/solution.java
@@ -4,7 +4,7 @@
 // Difficulty: Unknown
 // Language  : java
 // URL       : https://takeuforward.org/plus/dsa/problems/delete-the-middle-node-in-ll?subject=dsa&approach=optimal-approach&tab=submissions
-// Solved    : 2026-05-30T18:06:57.109Z
+// Solved    : 2026-05-30T18:07:00.921Z
 // ────────────────────────────────────────────────────────────
 
 class Solution {
