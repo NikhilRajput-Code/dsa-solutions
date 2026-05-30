d436583fdc4dc87987d45938a7d56e302daa9066
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-30 23:10:14 +0530

    ✅ Add TUF — Command Palette

--- TUF/command-palette/solution.java
+++ TUF/command-palette/solution.java
@@ -4,17 +4,18 @@
 // Difficulty: Unknown
 // Language  : java
 // URL       : https://takeuforward.org/plus/dsa/problems/delete-the-middle-node-in-ll?subject=dsa-concept-revision&tab=editorial&approach=optimal
-// Solved    : 2026-05-30T17:40:01.013Z
+// Solved    : 2026-05-30T17:40:11.012Z
 // ────────────────────────────────────────────────────────────
 
-    if(head == null & head.next == null) 
-    public ListNode deleteMiddle(ListNode head) {
- */
-
-class Solution {
     return null;
     ListNode slow = head;
     ListNode fast = head.next.next;
     while(fast != null && fast.next != null) {
         slow = slow.next;
         fast = fast.next.next;
+    }
+      slow.next = slow.next.next;
+      return head;
+     }
+}
+    if(head == null & head.next == null) 
