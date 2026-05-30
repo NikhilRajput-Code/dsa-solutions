2387cab96e7f3f5c70cc9447ff4820a5a9345f08
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-30 23:24:10 +0530

    ✅ Add LeetCode — Delete the Middle Node of a Linked List

--- LeetCode/delete-the-middle-node-of-a-linked-list/solution.java
+++ LeetCode/delete-the-middle-node-of-a-linked-list/solution.java
@@ -4,7 +4,7 @@
 // Difficulty: Medium
 // Language  : java
 // URL       : https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/description/
-// Solved    : 2026-05-30T17:53:30.352Z
+// Solved    : 2026-05-30T17:54:00.350Z
 // ────────────────────────────────────────────────────────────
 
 // Paste solution here.
