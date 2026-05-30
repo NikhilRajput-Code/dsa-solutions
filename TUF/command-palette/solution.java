98591cbd258cf4e007da2e5927932dacf6ce350e
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-30 23:03:41 +0530

    ✅ Add TUF — Command Palette

--- TUF/command-palette/solution.java
+++ TUF/command-palette/solution.java
@@ -4,7 +4,7 @@
 // Difficulty: Unknown
 // Language  : java
 // URL       : https://takeuforward.org/plus/dsa/problems/insertion-at-the-head-of-ll?subject=dsa&approach=optimal-approach&tab=submissions
-// Solved    : 2026-05-30T17:33:30.351Z
+// Solved    : 2026-05-30T17:33:40.217Z
 // ────────────────────────────────────────────────────────────
 
 }
