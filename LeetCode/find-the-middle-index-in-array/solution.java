f3350028f043e04eaabbaa0d6f0764dc554bdeb9
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-30 23:33:02 +0530

    ✅ Add LeetCode — Find the Middle Index in Array

---
+++ LeetCode/find-the-middle-index-in-array/solution.java
@@ -0,0 +1,14 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : LeetCode
+// Problem   : Find the Middle Index in Array
+// Difficulty: Easy
+// Language  : java
+// URL       : https://leetcode.com/problems/find-the-middle-index-in-array/solutions/8297806/ez-java-solution-by-aniketmore-qfwv/
+// Solved    : 2026-05-30T18:03:00.777Z
+// ────────────────────────────────────────────────────────────
+
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        
+    }
+}
