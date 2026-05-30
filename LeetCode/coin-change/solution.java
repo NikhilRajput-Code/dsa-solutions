f27c61520c987af4de9905bb5d3cfd5a3fc58e20
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-30 23:36:42 +0530

    ✅ Add LeetCode — Coin Change

---
+++ LeetCode/coin-change/solution.java
@@ -0,0 +1,14 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : LeetCode
+// Problem   : Coin Change
+// Difficulty: Medium
+// Language  : java
+// URL       : https://leetcode.com/problems/coin-change/solutions/8297522/java-solution-dp-bottom-up-approach-begi-xaoz/
+// Solved    : 2026-05-30T18:06:40.157Z
+// ────────────────────────────────────────────────────────────
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        
+    }
+}
