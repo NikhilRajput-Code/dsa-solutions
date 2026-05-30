16565a905c6e9c4a96f6bc2c65fe05a16621cab6
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-30 23:34:43 +0530

    ✅ Add LeetCode — Linked List Cycle II

---
+++ LeetCode/linked-list-cycle-ii/solution.java
@@ -0,0 +1,24 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : LeetCode
+// Problem   : Linked List Cycle II
+// Difficulty: Medium
+// Language  : java
+// URL       : https://leetcode.com/problems/linked-list-cycle-ii/description/
+// Solved    : 2026-05-30T18:04:40.561Z
+// ────────────────────────────────────────────────────────────
+
+            slow = slow.next;
+            fast = fast.next.next;
+        while(fast!=null && fast.next!=null) {
+        ListNode fast = head;
+        ListNode slow = head;
+    public ListNode detectCycle(ListNode head) {
+ */
+public class Solution {
+ *     }
+ * }
+ *         next = null;
+ *         val = x;
+ *     ListNode(int x) {
+ *     ListNode next;
+ *     int val;
