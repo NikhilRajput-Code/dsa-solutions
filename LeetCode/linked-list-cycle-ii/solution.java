943c55238244c1d382a26bd40c92f2e0b83dba02
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-30 23:34:54 +0530

    ✅ Add LeetCode — Linked List Cycle II

--- LeetCode/linked-list-cycle-ii/solution.java
+++ LeetCode/linked-list-cycle-ii/solution.java
@@ -3,22 +3,21 @@
 // Problem   : Linked List Cycle II
 // Difficulty: Medium
 // Language  : java
-// URL       : https://leetcode.com/problems/linked-list-cycle-ii/description/
-// Solved    : 2026-05-30T18:04:40.561Z
+// URL       : https://leetcode.com/problems/linked-list-cycle-ii/solutions/
+// Solved    : 2026-05-30T18:04:51.339Z
 // ────────────────────────────────────────────────────────────
 
-            slow = slow.next;
-            fast = fast.next.next;
-        while(fast!=null && fast.next!=null) {
-        ListNode fast = head;
-        ListNode slow = head;
-    public ListNode detectCycle(ListNode head) {
- */
 public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+ *         next = null;
  *     }
  * }
- *         next = null;
- *         val = x;
- *     ListNode(int x) {
- *     ListNode next;
+ */
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
  *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
