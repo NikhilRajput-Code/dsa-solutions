39d43d81e52e1308a2c902e2ddd6e33921e7dc5d
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-30 23:32:44 +0530

    ✅ Add LeetCode — Find the Middle Index in Array

---
+++ LeetCode/find-the-middle-index-in-array/solution.cpp
@@ -0,0 +1,10 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : LeetCode
+// Problem   : Find the Middle Index in Array
+// Difficulty: Easy
+// Language  : cpp
+// URL       : https://leetcode.com/problems/find-the-middle-index-in-array/description/
+// Solved    : 2026-05-30T18:02:41.097Z
+// ────────────────────────────────────────────────────────────
+
+// Paste solution here.
