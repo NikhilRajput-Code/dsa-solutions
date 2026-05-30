ea16747831cc1467598ac07d1becfc6abca4034d
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-31 00:05:42 +0530

    ✅ Add TUF — Command Palette

--- TUF/command-palette/solution.java
+++ TUF/command-palette/solution.java
@@ -4,7 +4,7 @@
 // Difficulty: Unknown
 // Language  : java
 // URL       : https://takeuforward.org/plus/dsa/problems/add-two-numbers-in-ll?subject=dsa&approach=optimal-approach&tab=submissions
-// Solved    : 2026-05-30T18:35:31.279Z
+// Solved    : 2026-05-30T18:35:41.277Z
 // ────────────────────────────────────────────────────────────
 
         ListNode temp = dummy;
