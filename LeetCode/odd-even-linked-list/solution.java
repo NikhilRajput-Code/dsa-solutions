c8637f817cc7603293af009bf4a7f7269a5159f7
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-31 10:24:51 +0530

    ✅ Add LeetCode — Odd Even Linked List

--- LeetCode/odd-even-linked-list/solution.java
+++ LeetCode/odd-even-linked-list/solution.java
@@ -3,19 +3,20 @@
 // Problem   : Odd Even Linked List
 // Difficulty: Medium
 // Language  : java
-// URL       : https://leetcode.com/problems/odd-even-linked-list/
-// Solved    : 2026-05-31T04:54:40.463Z
+// URL       : https://leetcode.com/problems/odd-even-linked-list/submissions/2017815541/
+// Solved    : 2026-05-31T04:54:50.768Z
 // ────────────────────────────────────────────────────────────
 
-    ListNode odd = head;
-    ListNode even = head.next;
-    ListNode evenHead = head.next;
-    while(even != null && even.next != null) {
-        odd.next = odd.next.next;
-        even.next = even.next.next;
-        odd = odd.next;
-        even = even.next;
-    }
-    odd.next = evenHead;
     return head;
     if(head == null || head.next == null) 
+    public ListNode oddEvenList(ListNode head) {
+class Solution {
+ */
+ * }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode() {}
+ *     ListNode next;
+ *     int val;
+ * public class ListNode {
+ * Definition for singly-linked list.
