d1e9aecff3728c913a2070861c2a99f3e1591f02
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-31 10:21:49 +0530

    ✅ Add LeetCode — Odd Even Linked List

---
+++ LeetCode/odd-even-linked-list/solution.cpp
@@ -0,0 +1,10 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : LeetCode
+// Problem   : Odd Even Linked List
+// Difficulty: Medium
+// Language  : cpp
+// URL       : https://leetcode.com/problems/odd-even-linked-list/description/
+// Solved    : 2026-05-31T04:51:47.571Z
+// ────────────────────────────────────────────────────────────
+
+// Paste solution here.
