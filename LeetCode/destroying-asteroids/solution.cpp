817906cbe1ae5ffdbf6d2aabbe0d72ff07c11921
NikhilRajput-Code <250231039@hbtu.ac.in>
2026-05-31 10:38:05 +0530

    ✅ Add LeetCode — Destroying Asteroids

---
+++ LeetCode/destroying-asteroids/solution.cpp
@@ -0,0 +1,10 @@
+// ────────────────────────────────────────────────────────────
+// Platform  : LeetCode
+// Problem   : Destroying Asteroids
+// Difficulty: Medium
+// Language  : cpp
+// URL       : https://leetcode.com/problems/destroying-asteroids/description/
+// Solved    : 2026-05-31T05:08:00.972Z
+// ────────────────────────────────────────────────────────────
+
+// Paste solution here.
